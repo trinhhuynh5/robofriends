e43fea6f48b5ae67070b8a1620c442a36963be6b
Trinh Huynh <trinhhuynh5@gmail.com>
2019-12-03 10:18:11 -0800

    Updates

---
+++ precache-manifest.4f73301cda37e3d4e7b37e8cbd45c298.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97cfcf9e42e07b3189c4f02659f0d1bd",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7e587ab3bd5998b37f91",
+    "url": "/robofriends/static/css/main.0ff520dc.chunk.css"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robofriends/static/js/2.fbad7c07.chunk.js"
+  },
+  {
+    "revision": "7e587ab3bd5998b37f91",
+    "url": "/robofriends/static/js/main.b9f55ecf.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
